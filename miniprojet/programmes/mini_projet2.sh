24945b45f2157b050e4ccd36adbc5cc5f36a72ff
GAdrianK <zagoradri@gmail.com>
2025-11-11 00:06:16 +0100

    enième modification, j'avais oublié  le compteur de mots en le supprimant involontairement

--- miniprojet/programmes/mini_projet2.sh
+++ miniprojet/programmes/mini_projet2.sh
@@ -9,7 +9,7 @@ out="${2:-tableaux/tableau-fr.html}" # permet de spécifier un fichier de sortie
 mkdir -p "$(dirname "$out")" # crée le répertoire de sortie s'il n'existe pas
 {
     echo "<html><body><table border=\"1\">" > "$out" # début du tableau HTML
-    echo "<tr><th>n</th><th>url</th><th>status</th><th>encoding</th></tr>" >> "$out" # en-têtes du tableau
+    echo "<tr><th>n</th><th>url</th><th>status</th><th>encoding</th><th>word count</th></tr>" >> "$out" # en-têtes du tableau
 }
 n=0
 while read -r line; do # ce bloc lit chaque ligne du fichier
@@ -17,5 +17,6 @@ while read -r line; do # ce bloc lit chaque ligne du fichier
     n=$((n + 1))
     status=$(curl -o /dev/null -s -w "%{http_code}\n" "$line")
     encoding=$(curl -s -I "$line" | grep -i "charset" | cut -d= -f2)
-    printf "<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td></tr>\n" "$n" "$line" "$status" "$encoding" >> "$out"
+    word_count=$(curl -s "$line" | wc -w)
+  printf "<tr><td>%d</td><td>%s</td><td>%s</td><td>%s</td><td>%d</td></tr>\n" "$n" "$line" "$status" "$encoding" "$word_count" >> "$out"
 done < "$file"
